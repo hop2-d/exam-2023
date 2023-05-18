46ad9557dfbf7fe175a08ea20a2076ceebd3b256
22PHOP2D-Duurenbayar <duurenbayarOwO@Nest-101s-iMac.local>
2023-05-18 19:12:49 +0800

    Duuree has done it AGAIN

---
+++ back-end/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connect = require("./db");
+const { testRouter } = require("./Routes/test.routes");
+
+const port = process.env.PORT || 7070;
+
+const app = express();
+
+connect();
+
+app.use(cors());
+app.use(express.json());
+app.use(testRouter);
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (_req, res) => {
+  res.send("Todo list backend");
+});
+
+app.listen(port, () => {
+  console.log(`🚀🚀🚀 Listening on port ${port} 🚀🚀🚀`);
+});
